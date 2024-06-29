ffaf89d27b2ddcb4e6b4a5cf089451ad2ecd3dc3
wekh <145914773+wekh@users.noreply.github.com>
2024-06-29 08:42:01 +0800

    Update get_iptv.py

--- .github/workflows/get_iptv.py
+++ .github/workflows/get_iptv.py
@@ -8,14 +8,18 @@ urls = [
 
 # 打开文件以写入模式
 with open("iptv.txt", "w") as file:
-    for url in urls:
+    for i, url in enumerate(urls):
         try:
             response = requests.get(url)
             response.raise_for_status()  # 检查请求是否成功
             playlist = response.text
 
+            # 如果不是第一个 URL，先写入一个换行符作为分隔符
+            if i > 0:
+                file.write("\n")
+            
             # 写入 URL 内容
-            file.write(playlist + "\n\n")  # 加两个换行符作为分隔符
+            file.write(playlist)
 
             print(f"内容已成功从 {url} 保存到 iptv.txt 文件中")
 
