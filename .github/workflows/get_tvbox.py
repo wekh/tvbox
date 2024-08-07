9368c85560fc7b8ddbb426f4fde089365f82e352
wekh <145914773+wekh@users.noreply.github.com>
2024-08-08 07:21:56 +0800

    Update get_tvbox.py

--- .github/workflows/get_tvbox.py
+++ .github/workflows/get_tvbox.py
@@ -31,7 +31,7 @@ try:
         cleaned_content_text = '\n'.join(cleaned_content)
 
         # 替换 "sites": [ 部分
-        new_sites_content = '''"sites": [
+        new_sites_content = '''  "sites": [
     {
       "key": "mfys",
       "name": "🍁免费┃不卡",
@@ -40,12 +40,20 @@ try:
       "searchable": 1,
       "quickSearch": 1,
       "filterable": 1
-    }
+    }，
   ]'''
+
+        # 调试输出清理后的内容
+        print("清理后的内容：")
+        print(cleaned_content_text)
 
         # 使用正则表达式找到并替换 "sites": [ 到下一个 ] 的内容
         if '"sites": [' in cleaned_content_text:
             cleaned_content_text = re.sub(r'"sites": \[.*?\]', new_sites_content, cleaned_content_text, flags=re.DOTALL)
+            print("替换后的内容：")
+            print(cleaned_content_text)
+        else:
+            print("未找到 '\"sites\": [' 需要替换。")
 
         # 解析内容
         data = json.loads(cleaned_content_text)
