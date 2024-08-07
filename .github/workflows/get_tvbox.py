19b55d18f7f85859fcb21218e2ca4f8de82599d3
wekh <145914773+wekh@users.noreply.github.com>
2024-08-08 07:12:33 +0800

    Update get_tvbox.py

--- .github/workflows/get_tvbox.py
+++ .github/workflows/get_tvbox.py
@@ -30,10 +30,26 @@ try:
         # 合并处理后的内容
         cleaned_content_text = '\n'.join(cleaned_content)
 
+        # 替换 "sites": [ 部分
+        new_sites_content = '''"sites": [
+    {
+      "key": "mfys",
+      "name": "🍁免费┃不卡",
+      "type": 1,
+      "api": "https://ys.wekh.eu.org/api.php/provide/vod/",
+      "searchable": 1,
+      "quickSearch": 1,
+      "filterable": 1
+    }
+  ]'''
+
+        # 使用正则表达式找到并替换 "sites": [ 到下一个 ] 的内容
+        cleaned_content_text = re.sub(r'"sites": \[.*?\]', new_sites_content, cleaned_content_text, flags=re.DOTALL)
+
         # 解析内容
         data = json.loads(cleaned_content_text)
 
-        # 修改内容
+        # 修改 lives 内容
         data["lives"] = [
             {
                 "name": "IPV6",
@@ -43,24 +59,6 @@ try:
                 "ua": "okhttp/3.15"
             }
         ]
-        # 替换 "sites": [ 部分
-        new_sites_content = '''"sites": [
-            {
-                "key": "mfys",
-                "name": "🍁免费┃不卡",
-                "type": 1,
-                "api": "https://ys.wekh.eu.org/api.php/provide/vod/",
-                "searchable": 1,
-                "quickSearch": 1,
-                "filterable": 1
-            }
-        ]'''
-
-        # 使用正则表达式找到并替换 "sites": [ 到下一个 ] 的内容
-        cleaned_content_text = re.sub(r'"sites": \[.*?\]', new_sites_content, cleaned_content_text, flags=re.DOTALL)
-
-        # 解析内容
-        data = json.loads(cleaned_content_text)
 
         # 将修改后的内容转换为 JSON 字符串，并指定 ensure_ascii=False 以确保汉字和表情符号正常显示
         modified_content = json.dumps(data, indent=2, ensure_ascii=False)
